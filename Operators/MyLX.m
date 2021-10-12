2f060f17eaf6db9a2ee06f262bd162530ab442e7
Julia <julia.heise.95@outlook.de>
2021-10-12 10:26:46 +0200

    improved laplace

--- Operators/MyLX.m
+++ Operators/MyLX.m
@@ -11,7 +11,7 @@ classdef MyLX < XOPERATOR
             if nargin > 2
                 [a,b] = deal(Parameter{:});
             else
-                [a,b] = deal(0,0.5);
+                [a,b] = deal(0,0.2);
             end
 
             if isa(Parentpool(1),'SOLUTION')
@@ -21,17 +21,26 @@ classdef MyLX < XOPERATOR
                 restructure = false;
             end
             
-            Parent1 = Parentpool(1:floor(end/2),:);
-            Parent2 = Parentpool(floor(end/2)+1:floor(end/2)*2,:);
             N = size(Parentpool, 1);
+            if(mod(N,2) == 0)
+                M = N/2;
+                Parent1 = Parentpool(1:M/2,:);
+                Parent2 = Parentpool(M/2+1:end,:);
+            else
+                M = ceil(N/2);
+                Parent1 = [Parentpool(1:M-1,:); Parentpool(randi(N,1,1),:)];
+                Parent2 = Parentpool(M:end,:);                
+            end
+            
 
             %% Offspring Generation
-            u = rand(N,1);
-            beta(u<=0.5) = a - b*reallog(u(u<=0.5));
-            beta(u>0.5) = a + b*reallog(u(u>0.5));
+            u = rand(M,1);
+            f = 1/2;
+            beta(u<=f) = a + b*(reallog(2 * u(u<=f)));
+            beta(u>f) = a - b*(reallog(2 * (1-u(u>f))));
 
-            Offspring = [Parent1 + beta(1:floor(end/2))' .* abs(Parent1-Parent2) % abs(Parent1-Parent2) 
-                Parent2 + beta(floor(end/2)+1:floor(end/2)*2)' .* abs(Parent1-Parent2)]; %abs(Parent1-Parent2) 
+            Offspring = [Parent1 + beta' .* (Parent1-Parent2)
+                Parent2 + beta' .* (Parent1-Parent2)];
 
             if(restructure)
                 Offspring = SOLUTION(Offspring,[], repelem(obj.TAG, N, 1));
--- VisualizeOperators.m
+++ VisualizeOperators.m
@@ -2,23 +2,22 @@ close all; clc;
 %% Visualize Operator results
 %% init
 Population = [0.3 0.4; 0.6 0.2; 0.5 0.7];
-
-% Operators = {MyLCX(), MyLX(), MyRSBX(), MySBX()};
-% for operator = Operators
-%     Pop = Population;
-%     f = figure('Name', operator{1}.TAG);
-%     axis([0 1 0 1]);
-%     hold on
-%     for i=1:500
-%         Pop = Pop(randperm(length(Pop)),:);
-%         x = operator{1}.Cross(Pop);
-%         scatter(x(:,1), x(:,2), 3, 'r', 'filled');
-%     end
-%     scatter(Population(:,1), Population(:,2), 'filled', 'b');
-%     hold off
-%     
-%     exportgraphics(f, operator{1}.TAG + '.png','Resolution',300)
-% end
+Operators = {MyLX()};
+for operator = Operators
+    Pop = Population;
+    f = figure('Name', operator{1}.TAG);
+    axis([0 1 0 1]);
+    hold on
+    for i=1:500
+        Pop = Pop(randperm(length(Pop)),:);
+        x = operator{1}.Cross(Pop);
+        scatter(x(:,1), x(:,2), 3, 'r', 'filled');
+    end
+    scatter(Population(:,1), Population(:,2), 'filled', 'b');
+    hold off
+    
+    exportgraphics(f, operator{1}.TAG + '.png','Resolution',300)
+end
 
 Population = [0.3 0.4; 0.6 0.2; 0.5 0.7];
 
@@ -59,7 +58,7 @@ scatter(y(:,1), y(:,2), 3, 's', 'filled');
 scatter(Population(:,1), Population(:,2), 'filled', 'b');
 hold off
 
-exportgraphics(f, Operator.TAG + '.png','Resolution',300)
+%exportgraphics(f, Operator.TAG + '.png','Resolution',300)
 
 
 
